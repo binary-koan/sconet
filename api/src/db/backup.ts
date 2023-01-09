c49f027e896f85f1d585c0cfb102a7076a1a2a5a
Jono <reason.koan@gmail.com>
2023-01-10 10:20:43 +1300

    Fix backup logic again and refactor

--- api/src/db/backup.ts
+++ api/src/db/backup.ts
@@ -2,6 +2,7 @@ import { file, write } from "bun"
 import { exec } from "child_process"
 import { existsSync, statSync } from "fs"
 import { db } from "./database"
+import { Repo } from "./repo"
 import { accountMailboxesRepo } from "./repos/accountMailboxesRepo"
 import { categoriesRepo } from "./repos/categoriesRepo"
 import { currenciesRepo } from "./repos/currenciesRepo"
@@ -27,58 +28,81 @@ export const backupDatabase = async () => {
     return
   }
 
-  const backupDetails: BackupDetails = existsSync("data/backup-details.json")
-    ? JSON.parse(await file("data/backup-details.json").text())
-    : { backupIndex: 0 }
+  const backupDetails = await readBackupDetails()
 
-  const databaseFiles = [db.filename, `${db.filename}-shm`, `${db.filename}-wal`]
-  if (
-    databaseFiles.every(
-      (path) =>
-        backupDetails.lastBackupAt && statSync(path).mtime >= new Date(backupDetails.lastBackupAt)
-    )
-  ) {
-    console.log(`[BACKUP] Database not modified, skipping`)
+  if (!databaseModified(backupDetails)) {
+    console.log("[BACKUP] Database not modified, skipping")
     return
   }
 
-  // Minio Node has bugs with Bun, so just use the command-line client instead
-  await execCommand(
-    `mc alias set backup https://${process.env.BUCKET_ENDPOINT} ${process.env.ACCESS_KEY_ID} ${process.env.SECRET_ACCESS_KEY}`
-  )
+  await setupMinio()
 
   backupDetails.backupIndex = (backupDetails.backupIndex + 1) % BACKUPS_TO_KEEP
   backupDetails.lastBackupAt = new Date().toISOString()
 
   for (const repo of REPOS_TO_BACKUP) {
-    let offset = 0
-
-    while (true) {
-      const rows = repo.findAll({ limit: 1000, offset })
-      const path = `data/backup-${repo.tableName}-${offset}.json`
-
-      await write(path, JSON.stringify(rows))
-
-      await execCommand(
-        `mc od if=${path} of=backup/${process.env.BUCKET_NAME}/backup-${backupDetails.backupIndex}/${path}`
-      )
-
-      if (rows.length < 1000) break
-
-      offset += 1000
-    }
+    await backupRepo(repo, backupDetails)
   }
 
-  console.log("[BACKUP] Success")
+  await storeBackupDetails(backupDetails)
 
-  await write("data/backup-details.json", JSON.stringify(backupDetails))
+  console.log("[BACKUP] Success")
+}
+
+const databaseModified = ({ lastBackupAt }: BackupDetails) => {
+  const databaseFiles = [db.filename, `${db.filename}-shm`, `${db.filename}-wal`]
+
+  if (!lastBackupAt) return true
+
+  return databaseFiles.some((path) => statSync(path).mtime >= new Date(lastBackupAt))
+}
+
+const setupMinio = async () => {
+  // Minio Node has bugs with Bun, so just use the command-line client instead
+  const command = `mc alias set backup https://${process.env.BUCKET_ENDPOINT}`
+
+  console.log(`[BACKUP] Executing ${command} ACCESS_KEY_ID SECRET_ACCESS_KEY`)
+  await execCommand(`${command} ${process.env.ACCESS_KEY_ID} ${process.env.SECRET_ACCESS_KEY}`)
+}
+
+const backupRepo = async (repo: Repo<any, any, any>, backupDetails: BackupDetails) => {
+  let offset = 0
+
+  while (true) {
+    const rows = repo.findAll({ limit: 500, offset })
+    const path = `data/backup-${repo.tableName}-${offset}.json`
+
+    await write(path, JSON.stringify(rows))
+
+    const command = `mc od if=${path} of=backup/${process.env.BUCKET_NAME}/backup-${backupDetails.backupIndex}/${path}`
+
+    console.log(`[BACKUP] Executing ${command}`)
+    await execCommand(command)
+
+    if (rows.length < 500) break
+
+    offset += 500
+  }
+}
+
+const readBackupDetails = async (): Promise<BackupDetails> => {
+  return existsSync(backupDetailsPath())
+    ? JSON.parse(await file(backupDetailsPath()).text())
+    : { backupIndex: 0 }
+}
+
+const storeBackupDetails = async (backupDetails: BackupDetails) => {
+  await write(backupDetailsPath(), JSON.stringify(backupDetails))
   await execCommand(
-    `mc od if=data/backup-details.json of=backup/${process.env.BUCKET_NAME}/backup-${backupDetails.backupIndex}/backup-details.json`
+    `mc od if=${backupDetailsPath()} of=backup/${process.env.BUCKET_NAME}/backup-${
+      backupDetails.backupIndex
+    }/backup-details.json`
   )
 }
 
+const backupDetailsPath = () => "data/backup-details.json"
+
 const execCommand = (command: string) => {
-  console.log(`[BACKUP] Executing ${command}`)
   return new Promise<void>((resolve, reject) => {
     const process = exec(command)
 
