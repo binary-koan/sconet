45f6848b0cdbd0e4daec1ba8429c224ac1f8c5f5
Jono Mingard <reason.koan@gmail.com>
2023-05-28 00:01:32 +1200

    Add another missing currency to ts-money

--- api/src/extraCurrencies.ts
+++ api/src/extraCurrencies.ts
@@ -9,3 +9,13 @@ Currencies.XPF = {
   code: "XPF",
   name_plural: "French Polynesian Francs"
 }
+
+Currencies.FJD = {
+  symbol: "FJ$",
+  name: "Fiji Dollar",
+  symbol_native: "FJ$",
+  decimal_digits: 2,
+  rounding: 0,
+  code: "FJD",
+  name_plural: "Fiji Dollars"
+}
