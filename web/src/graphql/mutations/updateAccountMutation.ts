c5cc3bb0a73e5680bfbcd5ddfe15e8adad6e5dc6
Jono Mingard <reason.koan@gmail.com>
2025-09-13 10:35:29 +0100

    Fix account update

--- web/src/graphql/mutations/updateAccountMutation.ts
+++ web/src/graphql/mutations/updateAccountMutation.ts
@@ -1,13 +1,10 @@
 import { UpdateAccountMutation, UpdateAccountMutationVariables } from "../../graphql-types"
 import { gql } from "../../utils/gql"
 import { MutationOptions, useMutation } from "../../utils/graphqlClient/useMutation"
-import { FullAccountFragment } from "../fragments/accountFragments"
 import { ACCOUNTS_QUERY } from "../queries/accountsQuery"
 import { GET_ACCOUNT_QUERY } from "../queries/getAccountQuery"
 
 const UPDATE_ACCOUNT_MUTATION = gql`
-  ${FullAccountFragment}
-
   mutation UpdateAccount($id: ID!, $input: AccountInput!) {
     accountUpdate(input: { id: $id, accountInput: $input }) {
       account {
