4908405ba6701da5a6d6fc291d08b8339e346365
Jono Mingard <reason.koan@gmail.com>
2025-10-19 13:42:36 +0100

    Fix duplicate uploads

--- app/graphql/mutations/transaction_create.rb
+++ app/graphql/mutations/transaction_create.rb
@@ -11,12 +11,7 @@ module Mutations
     argument :transaction_input, Types::TransactionInputType, required: true
 
     def resolve(transaction_input:)
-      receipt_images = transaction_input[:receipt_images]
       transaction = ::Transaction.new(**transaction_input)
-
-      if receipt_images.present?
-        transaction.receipt_images.attach(receipt_images)
-      end
       
       raise GraphQL::ExecutionError.new "Error creating transaction", extensions: transaction.errors.to_hash unless transaction.save
 
