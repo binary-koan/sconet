9fa2259bd00e964d7c70970b05e4484f3948cb9c
Jono Mingard <reason.koan@gmail.com>
2026-01-28 22:21:51 +0000

    React Native Reusables

--- expo/app/_layout.tsx
+++ expo/app/_layout.tsx
@@ -1,6 +1,12 @@
-import { Stack } from "expo-router";
-import './global.css';
+import { Stack } from "expo-router"
+import "./global.css"
+import { PortalHost } from "@rn-primitives/portal"
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack />
+      <PortalHost />
+    </>
+  )
 }
